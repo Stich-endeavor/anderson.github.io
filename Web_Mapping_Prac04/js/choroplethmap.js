18eae9f5820143ba8f4efa828d85dd70ae7ed18e
Anderson <65152181+Stich-endeavor@users.noreply.github.com>
2025-05-06 16:38:20 +0300

    Update choroplethmap.js

--- Web_Mapping_Prac04/js/choroplethmap.js
+++ Web_Mapping_Prac04/js/choroplethmap.js
@@ -1,10 +1,9 @@
 var map = L.map('map').setView([58.373523, 26.716045], 12);
 
-const osm = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
-});
+const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: 'OpenStreetMap contributors',
+})
   
 osm.addTo(map);
 
